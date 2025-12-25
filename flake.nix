341da9d0220f72bd9d6e3a296266d0ac2fe2ff29
TLSenZ <sven@zemp.email>
2025-12-26 00:17:38 +0100

    First Version of inventory home.ini File and Flake for Nix Development

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "OpenTofu dev shell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.ansible pkgs.git];
+          shellHook   = "echo 'Ready for Ansible'";
+        };
+      });
+}
